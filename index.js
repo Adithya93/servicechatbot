fd82afc181867d2a35aa414ccc372db44e897c2d
Adithya <ra102@duke.edu>
2017-01-31 22:47:09 -0500

    Modify to initiate conversation upon detection of change in user presence

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var SlackBot = require('slackbots');
- 
+
+var TOKEN = 'xoxb-135911413271-tP7MN7fpshJbiYiSbkmmgsZL';
+var ownName = 'wackie'; // TEMP, EXTRACT PROGRAMMATICALLY
 // create a bot 
 var bot = new SlackBot({
-    token: 'xoxb-135870068519-NUwU5m8VkKb0K6ic7RnFeGkc', // Add a bot https://my.slack.com/services/new/bot and put the token  
+    token: TOKEN, // Add a bot https://my.slack.com/services/new/bot and put the token  
     name: 'Wackie'
 });
 
@@ -25,42 +27,76 @@ var params = {
 
 bot.on('start', function() {
     console.log("Wacky is up!");
-    
-    var users = bot.getUsers();
-    var firstRealName = users["_value"]["members"].length > 0 ? users["_value"]["members"][0]["real_name"] : null;
-    var firstUserName = users["_value"]["members"].length > 0 ? users["_value"]["members"][0]["name"] : null;
-    if (firstRealName != null) {
-        console.log("First user in this group: " + firstRealName);
-    }
-    if (firstUserName != null) {
-        console.log("Username of current client: " + firstUserName);
-        currentUser = firstUserName;
-    }
-    // define channel, where bot exist. You can adjust it there https://my.slack.com/services  
-    bot.postMessageToChannel('general', 'yakshemesh!', params);
-    
-    // define existing username instead of 'user_name' 
-    //bot.postMessageToUser('ra102', 'booyakasha!', params); 
-    bot.postMessageToUser(firstUserName, "What's up " + (firstRealName || "friend") + "?", params);
-
-    bot.on('message', function(data) {
-        if (data["type"] == "message" && data["subtype"] != "bot_message") {
-            currentChannel = currentChannel || data["channel"];
-            console.log("Omg I got a message : " + data["text"]);
-            if (done) bot.postMessageToUser(currentUser, PLACEHOLDER, params);
-            else {
-                //bot.postMessageToUser(firstUserName, data["text"], params);
-                started ? processInput(data["text"]) : startQuestions();
+    //console.log("Users info: " + JSON.stingify(im.list(TOKEN)));
+                
+            var users = bot.getUsers();
+            console.log(JSON.stringify(users));
+            console.log("No. of users: " + users["_value"]["members"].length);
+            console.log("Groups: " + JSON.stringify(bot.getGroups()));
+            console.log("Channels: " + JSON.stringify(bot.getChannels()));
+            var firstRealName = users["_value"]["members"].length > 0 ? users["_value"]["members"][0]["real_name"] : null;
+            var firstUserName = users["_value"]["members"].length > 0 ? users["_value"]["members"][0]["name"] : null;
+            if (firstRealName != null) {
+                console.log("First user in this group: " + firstRealName);
             }
-        }
-        else if (data["subtype"] == "bot_message") {
-            console.log("I said " + data["text"]);
-        }
-        else {
-            console.log("Non-message event");
-        }
-    });
-    
+            if (firstUserName != null) {
+                console.log("Username of current client: " + firstUserName);
+                //currentUser = firstUserName;
+            }
+            // define channel, where bot exist. You can adjust it there https://my.slack.com/services  
+            bot.postMessageToChannel('general', 'yakshemesh!', params);
+
+            // define existing username instead of 'user_name' 
+            //bot.postMessageToUser('ra102', 'booyakasha!', params); 
+            
+            //bot.postMessageToUser(firstUserName, "What's up " + (firstRealName || "friend") + "?", params);
+
+            bot.on('message', function(data) {
+                if (data["type"] == "message" && data["subtype"] != "bot_message") {
+                    currentChannel = currentChannel || data["channel"];
+                    console.log("Omg I got a message : " + data["text"]);
+                    if (done) bot.postMessageToUser(currentUser, PLACEHOLDER, params);
+                    else {
+                        //bot.postMessageToUser(firstUserName, data["text"], params);
+                        started ? processInput(data["text"]) : startQuestions();
+                    }
+                }
+                else if (data["subtype"] == "bot_message") {
+                    console.log("I said " + data["text"]);
+                }
+                else if (data["type"] == "presence_change") {
+                    if (data["presence"] == "active") {
+                        var user = data["user"];
+                        console.log("User " + user + " is now available!");
+                        var foundUser = fml_getName(user);
+                        if (foundUser != ownName) {
+                            bot.postMessageToUser(foundUser, "Welcome back!", params);
+                            console.log("Resetting status");
+                            resetStatus();
+                            currentUser = foundUser;
+                            console.log("Set current user to " + currentUser);
+                        }
+                        //console.log("User is " + JSON.stringify(bot.getUserId(user)));
+                    }
+                    else {
+                        console.log("Presence change: " + data["presence"]);
+                        resetStatus();
+                    }
+                }
+                else if (data["type"] == "team_join") {
+                    var user = data["user"];
+                    console.log("Newly joined team member is " + JSON.stringify(user));
+                    currentUser = user["name"];
+                    resetStatus();
+                }
+                else {
+                    console.log("Other event type: " + data["type"]);
+                }
+            });
+        //}
+        //else console.log("User " + data["user"] + " is now away, igoring");
+
+    //});    
     // If you add a 'slackbot' property,  
     // you will post to another user's slackbot channel instead of a direct message 
     //bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' }); 
@@ -71,6 +107,13 @@ bot.on('start', function() {
         saveChannel(); //
     });
 });
+
+function resetStatus() {
+    state = 0;
+    started = false;
+    done = false;
+}
+
 
 function startQuestions() {
     started = true;
@@ -102,4 +145,17 @@ function showCompletion() {
 function saveChannel() {
     console.log("User's valid responses: " + JSON.stringify(validResponses));
     // save currentChannel to REDIS or MongoDB, whichever preferred
+}
+
+function fml_getName(userID) {
+    var users = bot.getUsers();
+    var members = users["_value"]["members"];
+    for (var index = 0; index < members.length; index ++) {
+        if (members[index]["id"] == userID) {
+            console.log("Found user by ID! Name is " + members[index]["name"]);
+            return members[index]["name"];
+        }
+    }
+    console.log("Unable to find name by id!");
+    return null;
 }
